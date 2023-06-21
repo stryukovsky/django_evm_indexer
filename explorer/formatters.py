07708e55958ea9240bfd5b0b5753bb398f214c83
dmitry <strukovsky1@gmail.com>
2023-06-21 11:23:14 +0400

    chore: minor improvement of layout

--- explorer/formatters.py
+++
@@ -1,7 +0,0 @@
-def shorten_tx_hash(tx_hash: str, digits: int = 20) -> str:
-    if tx_hash.startswith("0x"):
-        if len(tx_hash) != 66:
-            return tx_hash
-    else:
-        return tx_hash
-    return f"{tx_hash[: (digits - 2) // 2]}...{tx_hash[-(digits - 2) // 2:]}"
